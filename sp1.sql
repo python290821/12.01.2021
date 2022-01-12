b68620058aa97ed7793c6d19c66e3808d95ab4e0
micserv010821 <dotnet290320@gmail.com>
2022-01-12 21:10:18 +0200

    Update sp1.sql

--- sp1.sql
+++ sp1.sql
@@ -62,13 +62,15 @@ select * from sp_product(2.2, 3.5);
 --         constraint movies_countries_id_fk
 --             references countries
 -- );
-
-insert into movies (title, release_date, price)
-values ('batman returns', '2020-12-16 20:21:00', 45.5);
-insert into movies (title, release_date, price)
-values ('wonder woman', '2018-07-11 08:12:11', 125.5);
-insert into movies (title, release_date, price)
-values ('matrix resurrection', '2021-01-03 09:10:11', 38.7);
+-- insert into countries(name) values ('Israel');
+-- insert into countries(name) values ('USA');
+-- insert into countries(name) values ('JAPAN');
+-- insert into movies (title, release_date, price, country_id)
+-- values ('batman returns', '2020-12-16 20:21:00', 45.5, 3);
+-- insert into movies (title, release_date, price, country_id)
+-- values ('wonder woman', '2018-07-11 08:12:11', 125.5, 3);
+-- insert into movies (title, release_date, price, country_id)
+-- values ('matrix resurrection', '2021-01-03 09:10:11', 38.7, 4);
 
 drop function sp_movies_stat;
 CREATE or replace function sp_movies_stat(out min_price double precision,
@@ -87,4 +89,71 @@ select * from sp_movies_stat();
 
 -- targil:
 -- 1. create sp returns name of the most expansive movie
--- 2. create sp_num_of_movies return the number of movies
+
+drop function sp_movies_expensive_name;
+CREATE or replace function sp_movies_expensive_name(out most_expensive_movie_name text)
+language plpgsql AS
+    $$
+        DECLARE
+            max_price double precision := 0;
+        BEGIN
+            select max (price)
+            into max_price
+            from movies;
+
+            select title from movies
+            where price = max_price
+            into most_expensive_movie_name;
+        end;
+    $$;
+
+select * from sp_movies_expensive_name();
+-- 2. create sp_num_of_movies return the number of movies + number of countries
+
+drop function sp_count_movies_and_countries;
+CREATE or replace function sp_count_movies_and_countries(out count_movies_and_countries bigint)
+language plpgsql AS
+    $$
+        DECLARE
+            count_movies bigint := 0;
+            count_countries bigint := 0;
+        BEGIN
+            select count (*)
+            into count_movies
+            from movies;
+
+            select count (*)
+            into count_countries
+            from countries;
+
+            count_movies_and_countries = count_movies +  count_countries;
+        end;
+    $$;
+
+select * from sp_count_movies_and_countries();
+
+drop function sp_insert_movie;
+CREATE or replace function sp_insert_movie(_title text, _release_date timestamp,
+    _price double precision, _country_id bigint)
+    returns bigint
+language plpgsql AS
+    $$
+        DECLARE
+            new_id bigint := 0;
+        BEGIN
+            INSERT INTO movies(title, release_date, price, country_id)
+            values (_title, _release_date, _price, _country_id)
+            returning id into new_id;
+
+            return new_id;
+        end;
+    $$;
+
+select * from sp_insert_movie('Queen gambit', cast('2020-08-12' as timestamp)
+    , 87.1, 3);
+select * from sp_insert_movie('Eternals', cast('2020-05-21' as timestamp)
+    , 101.3, 1);
+
+-- sp_update_movie (movie_details, _id => update ) return 0
+-- execute func
+-- etgar:* write it without return value
